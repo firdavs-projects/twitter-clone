88e82d4990df2fd3d5550f46a84889154905e7d1
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-08-24 12:32:37 +0500

    feat: base bootstrap design

--- src/app/components/auth/auth.ts
+++ src/app/components/auth/auth.ts
@@ -1,35 +1,45 @@
-import { authTemplate, createAuthError } from './template';
+import {authTemplate, createAuthError, loginTemplate} from './template';
 import Button from "../Button";
 import authManager from "../../services/authManager";
 import Node from '../Node';
 import { getLogin, getRegistration } from '../../services/api';
 import { setLocalStorage } from '../../services/localStorage';
+import footerTemplate from "../footer/template";
 
 class Auth {
     private rootNode: HTMLElement;
 
     constructor() {
-        this.rootNode = document.createElement('main');
-        this.rootNode.className = 'auth';
+        this.rootNode = document.createElement('div');
+        // this.rootNode.className = 'text-center h-full';
     }
 
     public getTemplate(isRegisterRoute: boolean): HTMLElement {
         this.rootNode.textContent = '';
         const authForm = document.createElement('div');
-        authForm.classList.add('authForm');
+        authForm.classList.add('text-center', 'h-full', 'd-flex', 'flex-column', 'justify-content-center', 'align-items-center');
 
-        isRegisterRoute && authForm.insertAdjacentHTML('beforeend', authTemplate('firstName', 'text', 'First name'));
-        isRegisterRoute && authForm.insertAdjacentHTML('beforeend', authTemplate('lastName', 'text', 'Last name'));
+        this.rootNode.textContent = '';
+        // this.rootNode.insertAdjacentHTML('afterbegin', loginTemplate());
+        // return this.rootNode;
 
-        authForm.insertAdjacentHTML('beforeend', authTemplate('username', 'text', 'User name'));
-        authForm.insertAdjacentHTML('beforeend', authTemplate('password', 'password', 'Password'));
+        // isRegisterRoute && authForm.insertAdjacentHTML('beforeend', authTemplate('firstName', 'text', 'First name'));
+        // isRegisterRoute && authForm.insertAdjacentHTML('beforeend', authTemplate('lastName', 'text', 'Last name'));
+
+        authForm.insertAdjacentHTML('afterbegin', loginTemplate(isRegisterRoute));
+        // authForm.insertAdjacentHTML('beforeend', authTemplate('password', 'password', 'Password'));
 
         const btnWrapper = Node.setChild(authForm, 'div');
+        // authForm.insertAdjacentHTML('beforeend', btnWrapper)
         this.rootNode.append(authForm);
 
         if (isRegisterRoute) {
             const btn = new Button(btnWrapper, 'Registration');
-            btn.onclick(async () => {
+            // const btn = document.getElementById('auth-btn')
+            btn.addClass('btn')
+            btn.addClass('btn-lg')
+            btn.addClass('btn-primary')
+            btn?.onclick(async () => {
                 console.log('registration...');
                 const inputUserName = document.getElementById('username') as HTMLInputElement;
                 const inputPassword = document.getElementById('password') as HTMLInputElement;
@@ -51,7 +61,7 @@ class Auth {
                     } else {
                         console.log('Incorrect data');
                         errorElement ? errorElement.remove() : null;
-                        authForm.insertAdjacentHTML('beforeend', createAuthError('Incorrect data'));
+                        // authForm.insertAdjacentHTML('beforeend', createAuthError('Incorrect data'));
                     }
                 } catch (error) {
                     console.log(error);
@@ -61,6 +71,10 @@ class Auth {
 
         } else {
             const btn = new Button(btnWrapper, 'Login');
+            btn.addClass('btn')
+            btn.addClass('btn-lg')
+            btn.addClass('btn-primary')
+            // const btn = document.getElementById('auth-btn')
             btn.onclick(async () => {
                 console.log('logining...');
                 const inputUserName = document.getElementById('username') as HTMLInputElement;
@@ -75,12 +89,13 @@ class Auth {
                     } else {
                         console.log(`Incorrect username or password`);
                         errorElement ? errorElement.remove() : null;
-                        authForm.insertAdjacentHTML('beforeend', createAuthError('Incorrect username or password'));
+                        // authForm.insertAdjacentHTML('beforeend', createAuthError('Incorrect username or password'));
                     }
                 } catch (error) {
                     console.log(error);
                 }
             })
+
         }
         return this.rootNode;
     }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import './global.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css'; // will override bootstrap styles
+import 'bootstrap/dist/js/bootstrap.js';
 import app from './app/app';
 
 app.start();
