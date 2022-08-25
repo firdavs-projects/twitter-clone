d9a7d2fc4ef7c50faf778eb4a689a22ebce15c62
gavrilenkoartem <gavrilenkoartem038@gmail.com>
2022-08-25 17:30:44 +0400

    feat: add some validation

--- src/app/components/userProfile/templates.ts
+++ src/app/components/userProfile/templates.ts
@@ -3,7 +3,7 @@ class UserProfileTemplates {
         return `<div class="user-container container" id="${id}">
         <div class="head-part">
           <div class="image-part">${name.slice(0, 1).toUpperCase()}</div>
-          <button class="edit-user-button btn btn-primary btn-sm" data-id="${id}" data-bs-toggle="modal" data-bs-target="#editBackdrop">Edit profile</button>
+          <button class="edit-user-button btn btn-primary btn-sm" data-id="${id}" data-bs-target="#editBackdrop">Edit profile</button>
           ${this.createModalForm(this.editBody(), 'editBackdrop')}
           </div>
         <div class="user-data">
@@ -14,16 +14,16 @@ class UserProfileTemplates {
     };
 
     public createModalForm = (body: string, id: string) => {
-        return `<div class="modal fade" id="${id}" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        return `<div class="modal fade" id="${id}" tabindex="-1" aria-labelledby="modelLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="staticBackdropLabel">Edit profile</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Закрыть"></button>
+              <h5 class="modal-title" id="modelLabel">Edit profile</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">${body}</div>
             <div class="modal-footer">
-              <button class="save-profile-button btn btn-primary btn-sm" data-bs-dismiss="modal">Save changes</button>
+              <button class="save-profile-button btn btn-primary btn-sm">Save changes</button>
             </div>
           </div>
         </div>
@@ -32,19 +32,19 @@ class UserProfileTemplates {
 
     public editBody = () => {
         return `<div class="form-floating edit-container">
-      <input type="text" class="form-control edit-username" id="floatingUsername" placeholder="Username">
+      <input type="text" class="form-control edit-username" id="floatingUsername" placeholder="Username" minlength="3" maxlenght="40" required>
       <label for="floatingUsername">Username</label>
     </div>
     <div class="form-floating edit-container">
-      <input type="text" class="form-control edit-name" id="floatingName" placeholder="FirstName">
+      <input type="text" class="form-control edit-name" id="floatingName" placeholder="FirstName" minlength="3" maxlenght="30" required>
       <label for="floatingName">FirstName</label>
     </div>
     <div class="form-floating edit-container">
-      <input type="text" class="form-control edit-surname" id="floatingLastName" placeholder="LastName">
+      <input type="text" class="form-control edit-surname" id="floatingLastName" placeholder="LastName" minlength="3" maxlenght="30" required>
       <label for="floatingLastName">LastName</label>
     </div>
     <div class="form-floating edit-container">
-      <input type="text" class="form-control edit-phone" id="floatingPhone" placeholder="Phone">
+      <input type="text" class="form-control edit-phone" id="floatingPhone" placeholder="Phone" pattern="^[+]?[1-9]+[0-9]*$" max-length="15">
       <label for="floatingPhone">Phone</label>
     </div>`;
     };
@@ -69,7 +69,7 @@ class UserProfileTemplates {
           </div>
           <div class="post-text">${text}</div>
           <div class="post-edit">
-            <textarea class="post-input form-control"></textarea>
+            <textarea class="post-input form-control" minlength="3" maxlength="255" required></textarea>
             <button class="save-button btn btn-primary btn-sm" data-id="${id}">Save</button>
           </div>
           <div class="post-reactions">
--- src/app/components/userProfile/userProfile.ts
+++ src/app/components/userProfile/userProfile.ts
@@ -1,3 +1,4 @@
+import * as bootstrap from 'bootstrap';
 import {
     addLike,
     deleteLike,
@@ -16,6 +17,7 @@ const template = new UserProfileTemplates();
 
 class UserProfile {
     public rootNode: HTMLElement;
+    public modal: bootstrap.Modal | undefined;
 
     constructor() {
         this.rootNode = document.createElement('main');
@@ -26,6 +28,7 @@ class UserProfile {
         this.rootNode.innerHTML = '';
         await this.showUser();
         await this.showPosts();
+        this.modal = new bootstrap.Modal(<HTMLElement>document.getElementById('editBackdrop'));
     }
 
     public async showUser(): Promise<void> {
@@ -79,15 +82,20 @@ class UserProfile {
 
     public async editPost(className: string, id: string) {
         const post = <HTMLElement>document.getElementById(`${id}`);
-        post.classList.toggle('edit');
         const input = post.querySelector('.post-input') as HTMLInputElement;
         const textEl = post.querySelector('.post-text') as HTMLElement;
         if (className === 'edit-post') {
             input.value = textEl.textContent as string;
+            post.classList.add('edit');
         } else if (className === 'save-button') {
-            const text = input.value as string;
-            textEl.textContent = text;
-            await editPost(id, { text });
+            if (input.checkValidity()) {
+                post.classList.remove('edit');
+                const text = input.value as string;
+                textEl.textContent = text;
+                await editPost(id, { text });
+            } else {
+                (<HTMLElement>input.parentElement).classList.add('was-validated');
+            }
         }
     }
 
@@ -102,20 +110,27 @@ class UserProfile {
         const inputName = document.querySelector('.edit-name') as HTMLInputElement;
         const inputLastName = document.querySelector('.edit-surname') as HTMLInputElement;
         const inputPhone = document.querySelector('.edit-phone') as HTMLInputElement;
+        const inputArr = [inputUsername, inputName, inputLastName, inputPhone];
         if (className === 'edit-user-button') {
+            (this.modal as bootstrap.Modal).toggle();
             inputUsername.value = user.username;
             inputName.value = user.firstName;
             inputLastName.value = user.lastName;
             inputPhone.value = user.phone || '';
         }
         if (className === 'save-profile-button') {
-            await saveProfileInfo({
-                username: inputUsername.value,
-                firstName: inputName.value,
-                lastName: inputLastName.value,
-                phone: inputPhone.value,
-            });
-            this.showPage();
+            if (inputArr.some((el) => !el.checkValidity())) {
+                (<HTMLElement>inputName.closest('.modal-body')).classList.add('was-validated');
+            } else {
+                (this.modal as bootstrap.Modal).toggle();
+                await saveProfileInfo({
+                    username: inputUsername.value,
+                    firstName: inputName.value,
+                    lastName: inputLastName.value,
+                    phone: inputPhone.value,
+                });
+                this.showPage();
+            }
         }
     }
 
