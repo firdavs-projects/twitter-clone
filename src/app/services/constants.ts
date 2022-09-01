3ae01a2f880e85d80a7860a841f45cbaa053fe07
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-09-01 15:28:30 +0500

    Revert "Profile page"

--- src/app/services/constants.ts
+++
@@ -1,9 +0,0 @@
-export const baseUrl = 'https://twitter-clone-api.onrender.com/api';
-// export const baseUrl = 'http://localhost:4000/api';
-
-export enum ApiMethods {
-  POST = 'POST',
-  GET = 'GET',
-  PUT = 'PUT',
-  DELETE = 'DELETE',
-}
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './global.css'; // will override bootstrap styles
-// import 'bootstrap/dist/js/bootstrap.js';
-import * as bootstrap from 'bootstrap';
-import app from './app/app';
-
-app.start();
