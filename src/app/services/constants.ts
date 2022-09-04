28d69937260ea63745be85a18a3a83855d017124
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-09-04 11:31:54 +0500

    fix: baseUrl

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,5 +1,5 @@
-// export const baseUrl = 'https://twitter-clone-api.onrender.com/api';
-export const baseUrl = 'http://localhost:4000/api';
+export const baseUrl = 'https://twitter-clone-api.onrender.com/api';
+// export const baseUrl = 'http://localhost:4000/api';
 
 export const ADMIN = 'ADMIN';
 
